9e44e572baa29ec03a42c6d0f0036f1aaf4d4846
Krithika Kondapalli <krithika.kondapalli@gmail.com>
2026-01-20 11:21:11 -0500

    add some basic algorithm implementation

--- src/matcher.py
+++ src/matcher.py
@@ -38,11 +38,69 @@ def load_input(filepath):
         student_preferences.append([int(num) for num in string_student_preferences])
     
     print("Number of hospitals/students:", n)
-   # print("Hospital preferences:", hospital_preferences)
-   # print("Student preferences:", student_preferences)'
+    # print("Hospital preferences:", hospital_preferences)
+    # print("Student preferences:", student_preferences)
+
+    # check for edge cases as per instructions: empty file and 1 hospital/student, equal number of hospital and students
+    # call some matching function here later
+
+# krithika's brain dump - may need to fix later
+def checkPreferences(current_hospital, current_student, student_preferences):
+    # place holder for now
+    return True # True = student DOES prefer the current_hospital over their own match
+
+def run_gale_shapley(hospital_preferences, student_preferences):
+    # make a copy of the preferences as we are altering them (deleting after we've proposed)
+    h_pref = hospital_preferences
+    s_pref = student_preferences
+
+    # store the total of pairs = N
+    N = len(hospital_preferences)
+
+    # store the hospital's (proposer's) matches
+    hospital_matches = [-1] * N # ex: [-1, -1, 2] where hospital 3 is matched to student 2
+
+    # store the student's matches 
+    student_matches = [-1] * N # ex: [-1, 3, -1] where student 2 is matched to hospital 3
+    
+    # begin the loop
+    while(-1 in hospital_matches):
+        # choose the first occurrence of -1
+        current_hospital = hospital_matches.index(-1) + 1 # index = 0 -> hospital = 1
+
+        # find top student from current_hospital's preference list (pretend the list is a queue)
+        current_student = h_pref[0] # if we already proposed to them, they wont be in this list
+
+        # check if a is free
+        if(student_matches[current_student-1] == -1):
+            student_matches[current_student-1] = current_hospital
+        elif(checkPreferences(current_hospital, current_student, student_preferences) == True):
+            # set the student's old hospital to unmatched
+            old_hospital = student_matches[current_student-1]
+            hospital_matches[old_hospital] = -1
+            
+            # remove the student from the hospital's preferences so it doesn't propose to it again
+            h_pref.pop(0)
+
+            # set the student's new hospital match
+            student_matches[current_student-1] = current_hospital
+        else:
+            # rejection case need to work on
+            
+
+            
+
+            
 
 
-   # call some matching function here later
+
+
+
+
+
+
+
+    
 
    
 
